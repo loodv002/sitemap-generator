3a9b97387d74ee51a8e03bdaa9a59fdd695268fa
loodv002 <loodv002@gmail.com>
2024-12-22 19:17:38 +0800

    perf: improve generator string concatenation

--- generator/generator_xml.py
+++ generator/generator_xml.py
@@ -20,20 +20,22 @@ class SiteMap:
         self.urlset.append(url)
 
     def to_string(self):
-        xml = f'<?xml version="1.0" encoding="{self.encoding}"?>\n'
-        xml += f'<urlset xmlns="{self.xmlns}">\n'
+        xml_lines = []
+        xml_lines.append(f'<?xml version="1.0" encoding="{self.encoding}"?>')
+        xml_lines.append(f'<urlset xmlns="{self.xmlns}">')
+        
         for url in self.urlset:
-            xml += "  <url>\n"
-            xml += f"    <loc>{url['loc']}</loc>\n"
+            xml_lines.append("  <url>")
+            xml_lines.append(f"    <loc>{url['loc']}</loc>")
             if url['lastmod']:
-                xml += f"    <lastmod>{url['lastmod']}</lastmod>\n"
+                xml_lines.append(f"    <lastmod>{url['lastmod']}</lastmod>")
             if url['changefreq']:
-                xml += f"    <changefreq>{url['changefreq']}</changefreq>\n"
+                xml_lines.append(f"    <changefreq>{url['changefreq']}</changefreq>")
             if url['priority']:
-                xml += f"    <priority>{url['priority']}</priority>\n"
-            xml += "  </url>\n"
-        xml += "</urlset>"
-        return xml
+                xml_lines.append(f"    <priority>{url['priority']}</priority>")
+            xml_lines.append("  </url>")
+        xml_lines.append("</urlset>")
+        return '\n'.join(xml_lines)
 
     def save(self, filename):
         xml = self.to_string()
@@ -55,13 +57,13 @@ class SiteMapParallel:
 
     def to_string(self, urlset):
 
-        xml = ""
+        xml_lines = []
         for url, priority in urlset:
-            xml += "  <url>\n"
-            xml += f"    <loc>{url}</loc>\n"
-            xml += f"    <priority>{priority}</priority>\n"
-            xml += "  </url>\n"
-        return xml
+            xml_lines.append("  <url>")
+            xml_lines.append(f"    <loc>{url}</loc>")
+            xml_lines.append(f"    <priority>{priority}</priority>")
+            xml_lines.append("  </url>")
+        return '\n'.join(xml_lines)
 
 
     def worker(self, shared_list, index, pages, start, end):
@@ -88,14 +90,16 @@ class SiteMapParallel:
                 processes.append(p)
                 p.start()
 
-            xml = f'<?xml version="1.0" encoding="{self.encoding}"?>\n'
-            xml += f'<urlset xmlns="{self.xmlns}">\n'
+            xml_head = f'<?xml version="1.0" encoding="{self.encoding}"?>\n'
+            xml_head += f'<urlset xmlns="{self.xmlns}">\n'
             with open("sitemap_new_threads.xml", "w", encoding=self.encoding) as f:
-                f.write(xml)
-                for p in processes:
+                f.write(xml_head)
+                
+                for i, p in enumerate(processes):
                     p.join()
-                for i in range(process_num):
                     f.write(shared_list[i])
+                    f.write('\n')
+                
                 f.write("</urlset>")
 
 
